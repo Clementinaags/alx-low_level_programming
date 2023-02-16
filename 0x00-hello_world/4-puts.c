bbb5c461745dccfcc55ad05385a4829f8a2dd99a
Clementinaags <clementinaags1@gmail.com>
2023-02-16 08:43:48 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,6 @@
-#!/bin/bash
 #include <stdio.h>
-/**
- * main -Enty
- * Return: Always 0
- */
-int main(void)
-{
-	puts("\"Programming is like building a multi lingual puzzle");
-	return (0);
+
+int main() {
+	puts("Programming is like building a multilingual puzzle");
+	return 0;
+}
